a40623e4e0ff3d5cd4d8f5ce0183dc9c321e39bd
Mulusew <31984102+mulu-sew@users.noreply.github.com>
2020-09-22 16:08:58 -0400

    stting components, contextAPI

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 export const Error = () => {
-    return (
-        <div>
-            hello from error page
-        </div>
-    )
-}
+  return (
+    <Hero>
+      <Banner title='404' subtitle='page not found'>
+        <Link to='/' className='btn-primary'>
+          Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import Sevices from "../components/Services";
+import FeaturedRoom from '../components/FeaturedRooms'
 export default function Home() {
-  return <Hero hero="defaultHero">hello from home</Hero>;
+  return (
+    <>
+    <Hero>
+      <Banner title='luxurious rooms' subtitle='deluxerooms starting at $299'>
+        <Link to='/rooms' className='btn-primary'>
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
+    <Sevices />
+    <FeaturedRoom />
+    </>
+  );
 }
-
-
