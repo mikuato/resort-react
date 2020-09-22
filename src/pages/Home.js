7b953d31a7188fbeccb5a5002c03ee6606568222
Mulusew <31984102+mulu-sew@users.noreply.github.com>
2020-09-22 19:02:11 -0400

    setup Rooms,Loading,FeaturedRooms CT wz  contextApi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,19 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import Sevices from "../components/Services";
-import FeaturedRoom from '../components/FeaturedRooms'
+import FeaturedRoom from "../components/FeaturedRooms";
 export default function Home() {
   return (
     <>
-    <Hero>
-      <Banner title='luxurious rooms' subtitle='deluxerooms starting at $299'>
-        <Link to='/rooms' className='btn-primary'>
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
-    <Sevices />
-    <FeaturedRoom />
+      <Hero>
+        <Banner title='luxurious rooms' subtitle='deluxerooms starting at $299'>
+          <Link to='/rooms' className='btn-primary'>
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Sevices />
+      <FeaturedRoom />
     </>
   );
 }
